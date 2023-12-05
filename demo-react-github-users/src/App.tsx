0b00ab3793566bf57c71266f7bdde10a355ced1b
James Posey <James.C.Posey@gmail.com>
2023-12-05 18:13:55 -0500

    added Form component

--- demo-react-github-users/src/App.tsx
+++ demo-react-github-users/src/App.tsx
@@ -1,7 +1,10 @@
+import Form  from "./components/Form"
+
 function App() {
   return (
     <div>
       <p className="font-bold text-center pt-22">Wassup Y'all</p>
+      <Form />
     </div>
   )
 
